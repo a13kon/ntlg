b596745941132cb98b0c1aed617f2dc7b689c146
a13con <a13kon@yandex.ru>
2023-10-27 16:39:39 +0300

    async function

--- sum.js
+++ sum.js
@@ -4,3 +4,20 @@ const nums = process.argv.slice(2).map(n => +n);
 const sum = nums.reduce((acc, cur) => acc + cur, 0);
 
 console.log(`sum: ${sum}`);
+
+f();
+
+async function f() {
+    var promise = new Promise (function (resolve, reject){
+        setTimeout(() => { 
+            resolve("OK");
+            console.log("OK");
+        }, 2000);
+        
+    });
+
+    //var answer =  await promise;
+    promise.then(() => {console.log("then")});
+    var answer =  await promise;
+    console.log("last answer");
+};
