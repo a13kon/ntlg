9c6e73ee0bee53455e88c538d2d5aec6809b620e
a13con <a13kon@yandex.ru>
2023-11-17 16:09:36 +0300

    route library

--- index.js
+++ index.js
@@ -1,109 +1,26 @@
 const express = require('express');
-const { v4: uuid} = require('uuid');
+const getBook = require('./routes/getBook');
+const postBook = require('./routes/postBook');
+const getBook2 = require('./routes/getBook2');
 
-class Book {
-    constructor(
-        title = "", 
-        description = "", 
-        authors = "", 
-        favorite = "", 
-        fileCover = "", 
-        fileName = "",
-        id = uuid()) {
-            this.id = id;
-            this.title = title;
-            this.description = description;
-            this.authors = authors;
-            this.favorite = favorite;
-            this.fileCover = fileCover;
-            this.fileName = fileName;
-    };
-};
 
-const stor = {
-    book : [],
-};
+
 
 const email = {
     id : 1,
     mail : "test@mailru",
 }
 
-const app = express();
+app = express();
 app.use(express.json());
 
+app.stor = {
+    book : []
+};
 
-app.post('/api/user/login/', (req, res) => {
-    res.status(201);
-    res.json(email);
-});
-
-app.get('/api/books/', (req, res) => {
-    const {book} = stor;
-    res.json(book);
-});
-
-app.get('/api/books/:id', (req, res) => {
-    const {book} = stor;
-    const {id} = req.params;
-    const idx = book.findIndex(el => el.id === id);
-
-    if (idx !== -1) {
-        res.json(book[idx]);
-    } else {
-        res.status(404);
-        res.json('404 | not found');
-    };
-});
-
-app.post('/api/books/', (req, res) => {
-    const {book} = stor;
-    const {title, description, authors, favorite, fileCover, fileName} = req.body;
-
-    const newBook = new Book(title, description, authors, favorite, fileCover, fileName);
-    book.push(newBook);
-
-    res.status(201);
-    res.json(book);
-});
-
-app.put('/api/books/:id', (req, res) => {
-    const {book} = stor;
-    const {title, description, authors, favorite, fileCover, fileName} = req.body;
-    const {id} = req.params;
-    const idx = book.findIndex(el => el.id === id);
-
-    if (idx !== -1) {
-        book[idx] = {
-            ...book[idx],
-            title,
-            description,
-            authors,
-            favorite,
-            fileCover,
-            fileName
-        };
-
-        res.json(book[idx]);
-    } else {
-        res.status(404);
-        res.json('404 | not found');
-    };
-});
-
-app.delete('/api/books/:id', (req, res) => {
-    const {book} = stor;
-    const {id} = req.params;
-    const idx = book.findIndex(el => el.id === id);
-
-    if (idx !== -1) {
-        book.slice(idx, 1);
-    } else {
-        res.status(404);
-        res.json('404 | not found');
-    };
-});
-
+app.use('/api/books', getBook);
+app.use('/api/books', getBook2);
+app.use('/api/books', postBook);
 
 
 const PORT = process.env.PORT || 3000;
---
+++ routes/postBook.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const router = express.Router();
+const { v4: uuid} = require('uuid');
+
+class Book {
+    constructor(
+        title = "", 
+        description = "", 
+        authors = "", 
+        favorite = "", 
+        fileCover = "", 
+        fileName = "",
+        id = uuid()) {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.authors = authors;
+            this.favorite = favorite;
+            this.fileCover = fileCover;
+            this.fileName = fileName;
+    };
+};
+
+router.post('/', (req, res) => {
+    const {book} = app.stor;
+    const {title, description, authors, favorite, fileCover, fileName} = req.body;
+
+    const newBook = new Book(title, description, authors, favorite, fileCover, fileName);
+    book.push(newBook);
+
+    res.status(201);
+    res.json(book);
+});
+
+module.exports = router;
