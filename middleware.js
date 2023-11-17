b23968cc1f8ad6122edc9d4327139b46f50a6c5c
a13con <a13kon@yandex.ru>
2023-11-17 12:40:23 +0300

    multer

--- middleware.js
+++ middleware.js
@@ -3,14 +3,18 @@ const express = require('express');
 const logger = require('./middleware/logger');
 const indexRouter = require('./routes/index');
 const testRouter = require('./routes/testRoute');
+const demoRouter = require('./routes/demo');
 const error404 = require('./middleware/err-404');
 
 const app = express();
 
 app.use(logger);
 
+
+app.use('/public', express.static(__dirname+'/public'));
 app.use('/', indexRouter);
 app.use('/test', testRouter);
+app.use('/demo', demoRouter);
 
 app.use(error404);
 
