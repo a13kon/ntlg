a26eecc961d1808ce4a7e03a90a8f6486eef80e4
a13kon <a13kon@yandex.ru>
2023-11-19 14:27:41 +0300

    done

--- index.js
+++ index.js
@@ -5,23 +5,28 @@ const getBookById = require('./routes/getBookById');
 const delBook = require('./routes/delBook');
 const putBook = require('./routes/putBook');
 const postLogin = require('./routes/postLogin');
+const downloadBook = require('./routes/downloadBook');
 const error404 = require('./middleware/err-404');
+
+
+
 
 app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
 
 app.stor = {
     book : []
 };
 
+//app.use('/download', express.static(__dirname+'/booksFiles/1700388269561-book.png'));
+
 app.use('/api/user/login', postLogin);
 app.use('/api/books', getBook);
 app.use('/api/books', getBookById);
-app.use('/api/books', postBook);
 app.use('/api/books', delBook);
 app.use('/api/books', putBook);
-
+app.use('/api/books', postBook);
+app.use('/api/books', downloadBook);
 
 app.use(error404);
 
--- routes/downloadBook.js
+++ routes/downloadBook.js
@@ -2,18 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 
-router.delete('/:id', (req, res) => {
+router.get('/:id/download', (req, res) => {
     const {book} = app.stor;
     const {id} = req.params;
     const idx = book.findIndex(el => el.id === id);
-
-    if (idx !== -1) {
-        book.splice(idx, 1);
-        res.json('deleted');
+    
+    if (idx !== -1 && book[idx].fileBook !== "") {
+        res.download(book[idx].fileBook);
     } else {
         res.status(404);
-        res.json('404 | not found'); // переделать
+        res.json('404 | not found');
     }
 });
-
+   
 module.exports = router;
