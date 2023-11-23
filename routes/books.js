06e46b1056290270f0f4fc549c9fd17a1e4dcf9d
a13con <a13kon@yandex.ru>
2023-11-23 12:29:02 +0300

    исправлен чекбокс, исправлена загрузка файла, добавлена кнопка скачать

--- routes/books.js
+++ routes/books.js
@@ -25,7 +25,6 @@ router.post('/create', fileMulter.single('fileBook'),
 (req, res) => {
         const {book} = stor;
         const {title, desc, authors, favorite, fileCover, fileName} = req.body;
-        //console.log(req.body);
         const newBook = new Book(title, desc, authors, favorite, fileCover, fileName);
     
         if(req.file){
@@ -118,8 +117,7 @@ router.get('/:id/download', (req, res) => {
     if (idx !== -1 && book[idx].fileBook !== "") {
         res.download(book[idx].fileBook);
     } else {
-        res.status(404);
-        res.json('404 | not found');
+        res.redirect('/404');
     }
 });
 
