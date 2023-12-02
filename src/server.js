f259de46fcff7b7f60860a83385544ec2c9ef662
a13kon <a13kon@yandex.ru>
2023-12-02 18:28:14 +0300

    docker-hub

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/:name', (req, res) => {
+    const { name } = req.params;
+    
+    res.json({message : `Hello, ${name} !`});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
