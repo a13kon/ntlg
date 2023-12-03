f4a7d6964512579b85d701bc977b868921c853a6
a13kon <a13kon@yandex.ru>
2023-12-03 13:40:20 +0300

    docker-compose

--- src/server.js
+++ src/server.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const redis = require('redis');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const REDIS_URL = process.env.REDIS_URL || "localhost";
 
-app.get('/:name', (req, res) => {
+const client = redis.createClient({url: REDIS_URL});
+
+(async () => {
+    await client.connect()
+})()
+
+app.get('/:name', async (req, res) => {
     const { name } = req.params;
-    
-    res.json({message : `Hello, ${name} !!!`});
+
+    try {   
+    const cnt = await client.incr(name);
+    res.json({message : `Hello, ${name} !`, cnt});
+    } catch (e) {
+        res.json({errorcode: 500, errmsg: `redis error: ${e}`})
+    }
 });
 
 app.listen(PORT, () => {
