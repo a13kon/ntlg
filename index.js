10aa7ac9aa60d372e8e82dc7550ccafda7ba9eb4
a13kon <a13kon@yandex.ru>
2023-12-07 20:16:14 +0300

    mongo

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const errorMiddleware = require('./middleware/error');
+const todoApiRouter = require('./routes/api/todo');
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/todo', todoApiRouter);
+
+app.use(errorMiddleware);
+
+async function start(PORT, UrlDB) {
+    try {
+        await mongoose.connect('mongodb://root:example@localhost:27017', {
+            dbName: "demo"
+        });
+        app.listen(PORT);
+    } catch (e) {
+        console.log(e);
+    };
+};
+
+const UrlDB = process.env.UrlDB;
+const PORT = process.env.PORT || 3000;
+start(PORT, UrlDB);
